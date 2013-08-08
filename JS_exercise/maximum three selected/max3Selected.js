cf6a280f9f7695a6c73f0857a461ca1078dba663
name <name@email.com>
2013-08-08 11:32:12 +0530

    third exercise modified

--- JS_exercise/maximum three selected/max3Selected.js
+++ JS_exercise/maximum three selected/max3Selected.js
@@ -1,34 +1,33 @@
-// function check(obj) checks the id of the argument "obj", if its not "none" then it further checks
-// if the checkbox is checked/unchecked thereby increasing/decreasing the counter. If the counter 
-// reaches 4, it shows an alert message with the name of the days which are checked.
-// If the id of the "obj" is "none" it unchecks all other checkboxes.
+// counter is incremented/decremented on subsesequent checking/unchecking of a checkbox
+// days stores all the checkbox elements
 var counter = 0;
-function check(obj) {
-  var days = document.getElementsByName('days');
-  if (obj.id != "none") {
-    if (obj.checked == true) {
-      days[days.length-1].checked = false;
-      counter++;
-      if (counter == 4) {
-        obj.checked = false;
-        var store = [];
-        for (var i = 0; i < (days.length); i++) {
-          if(days[i].checked == true) {
-            store.push(days[i].id);
-          }
+var days = document.getElementsByName('days');
+// checkForMax3 checks if the counter has exceeded 3 and shows an alert message with
+// the name of the days which have been selected.
+function checkForMax3(obj) {
+  if (obj.checked == true) {
+    days[days.length-1].checked = false;
+    counter++;
+    if (counter == 4) {
+      obj.checked = false;
+      var store = [];
+      for (var i = 0; i < (days.length); i++) {
+        if(days[i].checked == true) {
+          store.push(days[i].id);
         }
-        alert("Only 3 days can be selected. You have selected "+ store[0] + ", " + store[1]+ " and " + store[2]);
-        counter = 3;
       }
-    }
-    else {
-      counter--;
+      alert("Only 3 days can be selected. You have selected "+ store[0] + ", " + store[1]+ " and " + store[2]);
+      counter = 3;
     }
   }
-  else if (obj.id == "none") {
+  else {
+    counter--;
+  }
+}
+// checkNone(value) is called when NONE is cheked to uncheck all other checkboxes. 
+function checkNone(value) {
     for (var i = 0; i < (days.length-1); i++) {
-      days[i].checked = false;
+      days[i].checked = value;
     }
     counter = 0;
-  }
 }
