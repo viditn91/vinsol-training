3cd05756c887b85e58bd9868399d192a292fcfb0
name <name@email.com>
2013-08-10 18:38:54 +0530

    corrections made!

--- JS_exercise/maximum three selected/max3Selected.js
+++ JS_exercise/maximum three selected/max3Selected.js
@@ -2,16 +2,17 @@
 // days stores all the checkbox elements
 var counter = 0;
 var days = document.getElementsByName('days');
+var dayNone = document.getElementsByName('dayNone')[0];
 // checkForMax3 checks if the counter has exceeded 3 and shows an alert message with
 // the name of the days which have been selected.
 function checkForMax3(obj) {
-  if (obj.checked == true) {
-    days[days.length-1].checked = false;
+  if (obj.checked) {
+    dayNone.checked = false;
     counter++;
     if (counter == 4) {
       obj.checked = false;
       var store = [];
-      for (var i = 0; i < (days.length); i++) {
+      for (var i = 0; i < days.length; i++) {
         if(days[i].checked == true) {
           store.push(days[i].id);
         }
@@ -26,7 +27,7 @@ function checkForMax3(obj) {
 }
 // checkNone(value) is called when NONE is cheked to uncheck all other checkboxes. 
 function checkNone(value) {
-    for (var i = 0; i < (days.length-1); i++) {
+    for (var i = 0; i < days.length; i++) {
       days[i].checked = value;
     }
     counter = 0;
