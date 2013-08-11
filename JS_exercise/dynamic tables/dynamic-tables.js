cc23450ef83c116ffa0a2d3b0625de2332c0513f
name <name@email.com>
2013-08-11 10:45:01 +0530

    corrections made!

--- JS_exercise/dynamic tables/dynamic-tables.js
+++ JS_exercise/dynamic tables/dynamic-tables.js
@@ -1,6 +1,8 @@
 // global pointer increases/decreases with addition/delition of rows
 // used to provide unique id(s) to rows, cols etc.
 var counter = 1;
+//number of coloums
+var colNum = 3;
 // funtion to create rows
 function createRow() {
   var myTable = document.getElementById('table');
@@ -8,82 +10,63 @@ function createRow() {
   var row = table.insertRow(myTableLength);
   var rowId = "row" + counter;
   row.id = rowId;
-  createColumns(rowId,row,"","");
+  createColumns(rowId, row, "", "");
   counter++;
 }
 //function to create columns 
 function createColumns(rowId,row,name,email) {
-  var cellName = row.insertCell(0);
-  cellName.id = "cellName" + rowId;
-  var inputName = document.createElement('input');
-  inputName.type = "text";
-  inputName.id = "inputName" + rowId;
-  inputName.value = name;
-  cellName.appendChild(inputName);
-
-  var cellEmail = row.insertCell(1);
-  cellEmail.id = "cellEmail" + rowId;
-  var inputEmail = document.createElement('input');
-  inputEmail.type = "email";
-  inputEmail.id = "inputEmail" + rowId;
-  inputEmail.value = email;
-  cellEmail.appendChild(inputEmail);
-  
-  var cellSave = row.insertCell(2);
-  cellSave.id = "cellSave" + rowId;
-  var inputSave = document.createElement('input');
-  inputSave.type = "button";
-  inputSave.value = "Save";
-  inputSave.id = "inputSave" + rowId;
-  inputSave.setAttribute("onClick","saveValue('" + rowId +"')");
-  cellSave.appendChild(inputSave);
+  var cell = [];
+  var type = ["text", "email", "button"]; 
+  var value = [name, email, "Save"];
+  for (var i = 0; i < colNum; i++ ) {
+    cell[i] = row.insertCell(i);
+    cell[i].id = "cell"+ i + rowId;
+    var input = document.createElement('input');
+    input.type = type[i];
+    input.id = "input"+ i + rowId;
+    input.value = value[i];
+    if (i == 2) {
+      input.setAttribute("onClick","saveValue('" + rowId + "')");
+    }
+    cell[i].appendChild(input);
+  }
 }
 //function to save the entered values
 function saveValue(rowId) {
-  var row = document.getElementById(rowId);
-  
-  var cellName = document.getElementById("cellName" + rowId);
-  var inputName = document.getElementById("inputName" + rowId);
-  var inputNameValue = inputName.value;
-  cellName.removeChild(inputName);
-  cellName.appendChild(document.createTextNode(inputNameValue));
-  
-  var cellEmail = document.getElementById("cellEmail" + rowId);
-  var inputEmail = document.getElementById("inputEmail" + rowId);
-  var inputEmailValue = inputEmail.value;
-  cellEmail.removeChild(inputEmail);
-  cellEmail.appendChild(document.createTextNode(inputEmailValue));
-
-  var cellSave = document.getElementById("cellSave" + rowId);
-  var inputSave = document.getElementById("inputSave" + rowId);
-  cellSave.removeChild(inputSave);
-
-  var linkEdit = document.createElement('a');
-  linkEdit.appendChild(document.createTextNode("Edit "));
-  linkEdit.id = "linkEdit" + rowId;
-  linkEdit.href = "#!";
-  linkEdit.setAttribute("onClick","editRow('" + rowId +"')");
-  cellSave.appendChild(linkEdit);
-
-  var linkDelete = document.createElement('a');
-  linkDelete.appendChild(document.createTextNode("/ Delete"));
-  linkDelete.id = "linkDelete" + rowId;
-  linkDelete.href = "#!";
-  linkDelete.setAttribute("onClick","deleteRow('"+ rowId +"')");
-  cellSave.appendChild(linkDelete);
+  for (var i = 0; i < colNum; i++) {
+    var row = document.getElementById(rowId);
+    var cell = document.getElementById("cell" + i + rowId);
+    var input = document.getElementById("input" + i + rowId);
+    if (i != 2) {
+      var inputValue = input.value;
+      cell.removeChild(input);
+      cell.appendChild(document.createTextNode(inputValue));
+    }
+    else {
+      cell.removeChild(input);
+      createLinks(cell, "Edit ", rowId, "editRow");
+      createLinks(cell, "/ Delete", rowId, "deleteRow");
+    }
+  }
+}
+//funtion to create links once the save button is clicked
+function createLinks(cell, name, rowId, action) {
+  var link = document.createElement('a');
+  link.appendChild(document.createTextNode(name));
+  link.href = "#!";
+  link.setAttribute("onClick",action + "('" + rowId +"')");
+  cell.appendChild(link);
 }
 //function to edit the saved values
 function editRow(rowId) {
   var row = document.getElementById(rowId);
-  var cellName = document.getElementById("cellName" + rowId);
-  var cellEmail = document.getElementById("cellEmail" + rowId);
-  var cellSave = document.getElementById("cellSave" + rowId);
-  var cellNameValue = cellName.textContent;
-  var cellEmailValue = cellEmail.textContent;
-  row.removeChild(cellName);
-  row.removeChild(cellEmail);
-  row.removeChild(cellSave);
-  createColumns(rowId,row,cellNameValue,cellEmailValue);
+  var value = [];
+  for (var i = 0; i < colNum; i++) {
+    var cell = document.getElementById("cell" + i + rowId);
+    value[i] = cell.textContent;
+    row.removeChild(cell);
+  }
+  createColumns(rowId,row,value[0],value[1]);
 }
 //function to delete rows
 function deleteRow(rowId) {
