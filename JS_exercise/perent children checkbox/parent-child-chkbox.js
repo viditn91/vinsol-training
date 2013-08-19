f6b46e66a5a40896f6993c5a8a0f406a6887d404
name <name@email.com>
2013-08-19 11:29:53 +0530

    modified!

--- JS_exercise/perent children checkbox/parent-child-chkbox.js
+++ JS_exercise/perent children checkbox/parent-child-chkbox.js
@@ -1,18 +1,19 @@
-function displayList(obj) {
-  var outerList = document.getElementById(obj.name + "Outer");
-  var innerList = document.getElementById(obj.name + "Inner");
-  var innerListContent = innerList.getElementsByClassName('inner');
-  if(obj.checked) {
-    action('block',true);
-  }else {
-    action('none',false);
+function DisplayList(obj) {
+  this.outerList = document.getElementById(obj.name + "Outer");
+  this.innerList = document.getElementById(obj.name + "Inner");
+  this.innerListContent = this.innerList.getElementsByClassName('inner');
+  this.displayContent(obj.checked);
+}
+
+DisplayList.prototype.displayContent= function(value)
+{
+  this.innerList.style.display = value == true ? 'block' : 'none';
+  this.outerList.scrollIntoView(true);
+  for (var i = 0; i < this.innerListContent.length; i++) {
+    this.innerListContent[i].checked = value; 
   }
-function action(displayType,value)
-  {
-    innerList.style.display = displayType;
-    outerList.scrollIntoView(true);
-    for (var i = 0; i < innerListContent.length; i++) {
-      innerListContent[i].checked = value; 
-    }
-  }
+} 
+
+function initiate(object) {
+  var element = new DisplayList(object);
 }
