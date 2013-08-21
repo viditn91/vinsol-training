1737618c6739c75b1b7da8655620e5664905dd8a
name <name@email.com>
2013-08-21 10:25:18 +0530

    modified

--- JS_exercise/domain-subdomain-matching/domain-sudomain-matching.js
+++ JS_exercise/domain-subdomain-matching/domain-sudomain-matching.js
@@ -6,16 +6,16 @@ function Match(formId){
 }
 //method of the class to extract the Domain/ Subdomain from URL
 Match.prototype.extract = function(e) {
-  var pattern = /((\w+)\.)?(\w+\.[a-z]{2,4})/i;
+  var pattern = /^(?:(?:https?|ftp|file)\:\/\/)?(?:www\.)?((\w+)\.)?(\w+\.[a-z]{2,4})\/?(?:\w+\?\=\#\$\^)?/i;
   if(pattern.test(newForm.urlField.value)) {
     var domain = RegExp.$3;
     var subdomain = RegExp.$2;
     if (!subdomain) {
       alert("DOMAIN: "+domain);
     }else{
-      alert("DOMAIN: "+domain+" and SUBDOMAIN:"+subdomain);
+      alert("DOMAIN: "+ domain +" and SUBDOMAIN:" + subdomain);
     }
-  }else{
+  } else {
     alert("invalid URL");
     e.preventDefault();
   }
