99bb2be52a2b4a9268d0031f111ab9b5aed7345c
name <name@email.com>
2013-08-22 11:04:22 +0530

    corrected

--- jQuery_exercise/traversing/js/sandbox.js
+++ jQuery_exercise/traversing/js/sandbox.js
@@ -2,19 +2,13 @@
 var $img = $('img[alt]').each( function (index) {
   console.log($(this).attr('alt'));
 });
-//selecting 'search' input text box and traversing up to the form
-var $form = $('input[name=q]').parent();
-$form.addClass('module');
+//selecting 'search' input text box and traversing up to the form and adding a class to it
+$('input[name=q]').closest('form').addClass('module');
 //selecting list item that has a class of 'current',removing the class and changing the class of
 // the next list item 
-var $listItem = $('#myList li.current');
-$listItem.removeClass('current');
-$listItem.next().addClass('current');
-//selecting the select element and traversing to the submit button
-var $selectBox = $('#specials select');
-var $submitButton = $selectBox.parent().next().children('input:submit');
+$('#myList li.current').removeClass('current').next().addClass('current');
+//Selecting the select element inside #specials and traversing to the submit button
+$('#specials select').closest('form').find('input[type=submit]');
 //Selecting the first list item in the #slideshow element; adding the class 
 // "current" to it, and then adding a class of "disabled" to its sibling elements.
-var $firstListItem = $('#slideshow li:first');
-$firstListItem.addClass('current');
-$firstListItem.siblings().addClass('disabled');
+$('#slideshow li:first').addClass('current').siblings().addClass('disabled');
