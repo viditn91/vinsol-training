e8197e708a8e6933a835cfcb4ba2a6636e689794
name <name@email.com>
2013-08-22 12:39:50 +0530

    corrected

--- JS_exercise/input url and openit/input-url-and-open.js
+++ JS_exercise/input url and openit/input-url-and-open.js
@@ -2,13 +2,13 @@
 function getUrl(){
   do {
     var url = prompt("enter url");
-    if (!url || !url.trim()){
+    if (!url.trim()){
       alert("entered value is empty or consists of only spaces");
     }
     else {
       window.open(url,'','height=450,width=400,location=0,scrollbars=0,menubar=0,status=0,titlebar=0,toolbar=0');
     }
-  }while(!url || !url.trim());
+  }while(!url.trim());
   return url;
 }
 //Creating an event
