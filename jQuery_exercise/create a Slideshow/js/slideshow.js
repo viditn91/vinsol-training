c6b9cc1774c8b4c035af07219d725e79f6af57ab
name <name@email.com>
2013-08-24 16:59:42 +0530

    added exercise on creating a slideshow

--- jQuery_exercise/create a Slideshow/js/slideshow.js
+++ jQuery_exercise/create a Slideshow/js/slideshow.js
@@ -4,23 +4,24 @@ $('body').prepend($('#slideshow'));
 // then fading it out and fading in the next one.
 //Any list-item with index greater than '0' is hidden
 $("#slideshow li:gt(0)").hide();
-setInterval( function() { 
-	$('#slideshow  li:first')
-	.fadeOut(0)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
+setInterval( 
+  function() {
+    $('#slideshow  li:first')
+      .fadeOut(0)
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo('#slideshow');
     index();
-}, 2000);
+  }, 2000);
 //Creating a <p> element to display the index of the slide
 $('<p id = "navigation"></p>').appendTo('#slideshow li');
 var i = 0;
 index();
 //function to display the index of the slide
 function index() {
-	i++;
-	var len = $('#slideshow li').length;
-	i = i > len ? 1 : i ;
-	$('#navigation').text( i + ' of ' + len );
+  i++;
+  var len = $('#slideshow li').length;
+  i = i > len ? 1 : i ;
+  $('#navigation').text( i + ' of ' + len );
 }
