d7b5848f9fa4aa96b1d338a8d2d043e1627f35e4
name <name@email.com>
2013-08-28 11:14:47 +0530

    modified

--- JS_exercise/mindGame/mindGame.js
+++ JS_exercise/mindGame/mindGame.js
@@ -18,132 +18,115 @@ var jsonData = [{'url':'image_0.jpg'},
   {'url':'image_16.jpg'},
   {'url':'image_17.jpg'}];
 //Class or Constructor Function
-//correctMatches provides the current score in the game
-//for checking the code it can be set to 17 and result can be obtained by matching one pair 
 function MindGame() {
   scope = this;
-  this.myTable = document.getElementById('table');
-  this.button  = document.getElementById('start');
-  this.timeSpan = document.getElementById('time');
-  this.result = document.getElementById('result');
-  this.store = new Array(2);
-  this.flag = 0; 
-  correctMatches = 0;
-  this.button.addEventListener("click", this.begin.bind(this));
-  this.myTable.addEventListener("click", returnClick);
+  this.rowCount = 6;
+  this.gridSize = (this.rowCount * this.rowCount);
+  this.table    = document.getElementById('table');
+  this.button   = document.getElementById('start');
+  this.delay = 2000;  
+  this.button.addEventListener("click", this.displayGrid.bind(this));
+  this.table.addEventListener("click", target);
 }
-//class method to generate a random number
-MindGame.prototype.generateNumber = function(max, min) {
+//Class method to generate a random number
+MindGame.prototype.generateRandomNumber = function(max, min) {
   var randomNumber = (Math.floor(Math.random() *(max - min + 1) ) + min);
-  return randomNumber;
+  return randomNumber; 
 }
-//class method to generate an integer array of random numbers from 0-17 
-//without any repeatition
-MindGame.prototype.generateArray = function() {
-  var numberArray = new Array(0);
-  var num = 0, 
-      search = 0,
-      j = 0;
-  while (j <= 17) {
-    num  = this.generateNumber(17,0);
-    search = numberArray.contains(num);
-    if (!search){
-      numberArray.push(num);
-      j++;
+//Class method to display the grid of images
+MindGame.prototype.displayGrid = function() {
+  this.numberArray = new Array(this.gridSize);
+  for (var i = 0; i < this.gridSize; i++) {
+    this.numberArray[i] = i;
+    if(i >= this.gridSize/2) {
+      this.numberArray[i] = (i - this.gridSize/2)
     }
-  } return numberArray;
-}
-//method of Array to check if it contains the desired element
-Array.prototype.contains = function(obj) {
-  var i = this.length;
-  while (i--) {
-    if (this[i] == obj) {
-      return true;
-    }
-  } return false;
-}
-//method of the Class to generate a final concatinated array from two gerenerated arrays
-MindGame.prototype.generateFinalArray = function() {
-  var array1 = this.generateArray();
-  var array2 = this.generateArray();
-  this.finalArray = array1.concat(array2);
-}
-//method of the Class to append images in 'td' elements through the JSON
-MindGame.prototype.appendImagaes = function() {
-  td = document.getElementsByTagName('td');
-  for (var i in td) {
-    if(i <= 35) { 
-      var img = document.createElement('img');
-      var jsonValue = this.finalArray[i];
-      img.src = jsonData[jsonValue]['url'];
-      td[i].appendChild(img);
-    }
+  } for (var i = 0; i < this.gridSize/2; i++){
+    this.reShuffle();
   }
-}
-//method of the Class to start the timer and display the table once start button is clicked
-MindGame.prototype.begin = function(){
-  this.onStart();
-  this.outerTimer();
-}
-//handler function for the click event, returns target element i.e. 'td'
-var returnClick = function(e) {
-  scope.showImage(e.target);
-};
-
-MindGame.prototype.onStart = function() {
+  this.createCellElements();
   this.button.style.display = "none";
-  this.myTable.style.display = "block";
-  this.counter = 0;
-  this.store.splice(0,2);
+  this.table.style.display  = "block";
+  this.timer();
 }
-//method of the class to show the images which are clicked 
-//no more than 2 images are shown
-MindGame.prototype.showImage = function(cell) {
-  if(this.flag == 0){
-    this.counter++;
-    var img = cell.getElementsByTagName('img')[0];
-    img.style.display = "block";
-    this.store.push(img);
-    if(this.counter == 2) {
-      this.flag = 1;
-      this.compareImages();
-    } 
+//Class method to reshuffle the array of numbers
+MindGame.prototype.reShuffle = function() {
+  var num1 = this.generateRandomNumber((this.gridSize)-1, 0);
+  var num2 = this.generateRandomNumber((this.gridSize)-1, 0);
+  var temp = this.numberArray[num1];
+  this.numberArray[num1] = this.numberArray[num2];
+  this.numberArray[num2] = temp;
 }
-//the two clicked images are compared and hidden again if no match is found
-MindGame.prototype.compareImages = function() {
-  if(this.store[0].getAttribute('src') != this.store[1].getAttribute('src')) {
-    var timerId = setTimeout(function(){
-      scope.store[0].style.display = "none";
-      scope.store[1].style.display = "none";
-      scope.flag = 0;
-      scope.onStart();
-      }, 2000);   
-  } else {
-    this.flag = 0;
-    this.onStart();
-    correctMatches++;
-    this.result.textContent = "Score: " + correctMatches + " on 18";
-    if(correctMatches == 18) {
-      this.showResult();
-    }
+
+MindGame.prototype.createCellElements = function() {
+  for (var i = 0; i < this.rowCount; i++) {
+    var row = this.table.insertRow(i);
+    for(var j = 0; j < this.rowCount; j++) {
+      var cell = row.insertCell(j);
+      var img  = document.createElement('img');
+      var index = this.numberArray[j + (i*this.rowCount)];
+      img.src = jsonData[index]['url'];
+      cell.appendChild(img);
+      img.id = index;
     }
   }
 }
-//method of function to start the timer and dislay the time taken and score
-MindGame.prototype.outerTimer = function() {
-  this.timeSpan.style.display = "block";
-  this.result.style.display   = "block";
-  this.interval = setInterval('increment()',1000);
+//handler function for the click event
+var target = function(e) {
+  var img = e.target.childNodes[0];
+  img.className = "clicked";
+  scope.matchImages();
+};
+//method to match the two clicked images
+MindGame.prototype.matchImages = function () {
+  var img = document.getElementsByClassName('clicked');
+  if (img.length == 1) {
+    img[0].style.display = "block";
+  }else if(img.length == 2) {
+    var img1 = img[0];
+    var img2 = img[1];
+    img1.style.display = "block";
+    img2.style.display = "block";
+    if(img1.id == img2.id){
+      img1.className = "match";
+      img2.className = "unclicked";
+    }else {
+      this.incorrectMatch(img1,img2);
+    }
+  }else {
+      img[2].className = "unclicked";
+  }
 }
-//method of the class to show the final time taken and hide other elements
-MindGame.prototype.showResult = function() {
+//method to give a time delay when incorrect match is found
+MindGame.prototype.incorrectMatch = function(img1,img2) {
+  var timerId = setTimeout(function(){
+    img1.style.display = "none";
+    img2.style.display = "none";
+    img1.className = "unclicked";
+    img2.className = "unclicked";
+  }, this.delay);
+}
+
+MindGame.prototype.updateScore = function() {
+  var img = document.getElementsByClassName('match');
+  return img.length;
+}
+
+MindGame.prototype.finalResult = function() {
   clearInterval(this.interval);
-  this.timeSpan.textContent += "  - Puzzle Solved!" 
-  this.myTable.style.display = "none";
-  this.result.style.display = "none";
+  this.clock.textContent += "  - Puzzle Solved!" 
+}
+
+//method of function to start the timer
+MindGame.prototype.timer = function() {
+  this.clock    = document.getElementById('clock');
+  this.score    = document.getElementById('score');
+  this.clock.style.display = "block";
+  this.score.style.display    = "block";
+  this.interval = setInterval('updateTimer()',1000);
 }
 var seconds = 0;
-function increment() {
+function updateTimer() {
   seconds++;
   var minutes = Math.floor(seconds / 60);
   var localSeconds = (seconds -(minutes * 60));
@@ -153,10 +136,11 @@ function increment() {
   if( minutes <= 9) {
     minutes = "0" + minutes;
   }
-  scope.timeSpan.textContent = "Time Taken: " + minutes + " : " + localSeconds;
-  scope.result.textContent = "Score: " + correctMatches + " on 18";
+  scope.clock.textContent = "Time Taken: " + minutes + " : " + localSeconds;
+  scope.score.textContent = "Score: " + scope.updateScore();
+  if(scope.updateScore() == scope.gridSize/2) {
+    scope.finalResult();
+  }
 }
-//creating instance of Class
-var element = new MindGame();
-element.generateFinalArray();
-element.appendImagaes();
+//instance of Class
+var game = new MindGame();
