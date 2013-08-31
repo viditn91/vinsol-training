cc0c02f466483123bdb75d0b9e8ea3dfac5b742e
name <name@email.com>
2013-08-31 13:14:58 +0530

    smooth tranisitions. .

--- jQuery_exercise/create a Slideshow/js/slideshow.js
+++ jQuery_exercise/create a Slideshow/js/slideshow.js
@@ -9,15 +9,16 @@ var i   = 0;
 slideChange();
 setInterval( function () {
   slideChange();
-}, 2000);
+}, 3000);
 //Funtion to perform the animation and display the index of each image 
 function slideChange() {
   $('#slideshow li').find('#navigation')
                     .text(i+1 + ' of ' + len);
-  $('#slideshow li').eq(i)
-                    .fadeIn()
-                    .delay(1000)
-                    .fadeOut(1000)
+  $('#slideshow li').hide()
+                    .eq(i)
+                    .fadeIn(700,'linear')
+                    .delay(1600)
+                    .fadeOut(700,'linear')
                     .siblings()
                     .hide();
   i++;
