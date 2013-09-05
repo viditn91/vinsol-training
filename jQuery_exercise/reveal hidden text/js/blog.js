43e8615f7b1c7109f728f26813ea6b521868d0eb
name <name@email.com>
2013-09-05 10:20:32 +0530

    replaced bind() with on()

--- jQuery_exercise/reveal hidden text/js/blog.js
+++ jQuery_exercise/reveal hidden text/js/blog.js
@@ -1,16 +1,12 @@
 $('#blog p.excerpt').animate({'height':'130px'});
 //Adding effect so that clicking on a headline in the #blog div slides down the excerpt paragraph
-$('#blog h2').bind({
-  'click' : function() {
+$('#blog h2').on('click', function () {
     $('#blog p.excerpt').slideDown(1000);
-  }
 });
 //Clicking on another headline slides down its excerpt paragraph, and slide up any other 
 //currently showing excerpt paragraphs.
-$('#blog a').bind({
-  'click' : function(e) {
+$('#blog a').on('click', function (e) {
     e.preventDefault();
     $(this).closest('li').find('p.excerpt').slideDown(1000);
     $(this).closest('li').siblings().find('p.excerpt').slideUp(1000);
-  }
 });
