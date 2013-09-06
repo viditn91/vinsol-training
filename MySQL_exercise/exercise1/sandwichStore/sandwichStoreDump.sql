79d2fa52453f6eaed1a702d6e1917d46338ad597
name <name@email.com>
2013-09-06 11:11:04 +0530

    added first exercise

---
+++ MySQL_exercise/exercise1/sandwichStore/sandwichStoreDump.sql
@@ -0,0 +1,99 @@
+-- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: sandwich_store
+-- ------------------------------------------------------
+-- Server version	5.5.32-0ubuntu0.12.10.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `locations`
+--
+
+DROP TABLE IF EXISTS `locations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `locations` (
+  `lname` varchar(10) DEFAULT NULL,
+  `phone` varchar(10) DEFAULT NULL,
+  `address` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `locations`
+--
+
+LOCK TABLES `locations` WRITE;
+/*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+INSERT INTO `locations` VALUES ('lincoln','683 4523','lincoln place'),('o\'neill\'s','674 2314','pearse street'),('old nag','767 8132','dame st'),('buttery','702 3421','college st');
+/*!40000 ALTER TABLE `locations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sandwiches`
+--
+
+DROP TABLE IF EXISTS `sandwiches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sandwiches` (
+  `location` varchar(10) DEFAULT NULL,
+  `bread` varchar(10) DEFAULT NULL,
+  `filling` varchar(10) DEFAULT NULL,
+  `price` decimal(3,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sandwiches`
+--
+
+LOCK TABLES `sandwiches` WRITE;
+/*!40000 ALTER TABLE `sandwiches` DISABLE KEYS */;
+INSERT INTO `sandwiches` VALUES ('lincoln','rye','ham',1.25),('o\'neill\'s','white','cheese',1.20),('o\'neill\'s','whole','ham',1.25),('old nag','rye','beef',1.35),('buttery','white','cheese',1.00),('o\'neill\'s','white','turkey',1.35),('buttery','white','ham',1.10),('lincoln','rye','beef',1.35),('lincoln','white','ham',1.30),('old nag','rye','ham',1.40);
+/*!40000 ALTER TABLE `sandwiches` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tastes`
+--
+
+DROP TABLE IF EXISTS `tastes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tastes` (
+  `name` varchar(10) DEFAULT NULL,
+  `filling` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tastes`
+--
+
+LOCK TABLES `tastes` WRITE;
+/*!40000 ALTER TABLE `tastes` DISABLE KEYS */;
+INSERT INTO `tastes` VALUES ('brown','turkey'),('brown','beef'),('brown','ham'),('jones','cheese'),('green','beef'),('green','turkey'),('green','cheese');
+/*!40000 ALTER TABLE `tastes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-09-06  6:38:05
