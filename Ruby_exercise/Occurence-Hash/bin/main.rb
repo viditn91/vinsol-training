d132dc8083a4029e9eff2c14fb0f5a91ea74555b
name <name@email.com>
2013-09-18 08:02:18 +0530

    minor changes

--- Ruby_exercise/Occurence-Hash/bin/main.rb
+++ Ruby_exercise/Occurence-Hash/bin/main.rb
@@ -1,4 +1,4 @@
-string = "the quick brown fox jumps over a lazy dog".delete(' ')
+string = "the quick BROWN fox jumps over 23 lazy dogs".downcase
 alphabet_hash = Hash.new(0)
-string.each_char { |n| alphabet_hash[n] += 1 }
+string.each_char { |char| alphabet_hash[char] += 1 if char =~ /[a-z]/ }
 p alphabet_hash.sort
