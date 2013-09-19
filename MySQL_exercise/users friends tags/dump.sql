1f4fc908f015843a178458478d044ce7db1f90bb
name <name@email.com>
2013-09-19 18:56:35 +0530

    corrected

--- MySQL_exercise/users friends tags/dump.sql
+++ MySQL_exercise/users friends tags/dump.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (i686)
 --
--- Host: localhost    Database: users_friends
+-- Host: localhost    Database: users_tags
 -- ------------------------------------------------------
 -- Server version	5.5.32-0ubuntu0.12.10.1
 
@@ -23,9 +23,9 @@ DROP TABLE IF EXISTS `friends`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `friends` (
-  `user_id` varchar(10) DEFAULT NULL,
-  `friend` varchar(10) DEFAULT NULL,
-  KEY `user_id` (`user_id`),
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `friend` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`,`friend`),
   KEY `friend` (`friend`),
   CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `friends_ibfk_2` FOREIGN KEY (`friend`) REFERENCES `users` (`id`)
@@ -38,7 +38,7 @@ CREATE TABLE `friends` (
 
 LOCK TABLES `friends` WRITE;
 /*!40000 ALTER TABLE `friends` DISABLE KEYS */;
-INSERT INTO `friends` VALUES ('usera','userc'),('usera','userd'),('usera','usere');
+INSERT INTO `friends` VALUES (2,1),(3,1),(9,1),(1,2),(4,2),(6,2),(9,2),(1,3),(5,3),(8,3),(2,4),(6,4),(9,4),(3,5),(6,5),(8,5),(9,5),(2,6),(4,6),(5,6),(8,6),(9,6),(8,7),(3,8),(5,8),(6,8),(7,8),(1,9),(2,9),(4,9),(5,9),(6,9);
 /*!40000 ALTER TABLE `friends` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -50,12 +50,12 @@ DROP TABLE IF EXISTS `images`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `images` (
-  `id` varchar(10) NOT NULL DEFAULT '',
-  `image_user` varchar(10) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `image_user` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `image_user` (`image_user`),
   CONSTRAINT `images_ibfk_1` FOREIGN KEY (`image_user`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,7 +64,7 @@ CREATE TABLE `images` (
 
 LOCK TABLES `images` WRITE;
 /*!40000 ALTER TABLE `images` DISABLE KEYS */;
-INSERT INTO `images` VALUES ('img3','usera'),('img2','userc'),('img4','userc'),('img1','userd'),('img5','userd'),('img6','usere');
+INSERT INTO `images` VALUES (1,1),(4,2),(8,2),(3,3),(6,5),(5,8),(2,9),(7,9),(9,9);
 /*!40000 ALTER TABLE `images` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -76,8 +76,8 @@ DROP TABLE IF EXISTS `tags`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tags` (
-  `image_id` varchar(10) DEFAULT NULL,
-  `tagged` varchar(10) DEFAULT NULL,
+  `image_id` int(11) DEFAULT NULL,
+  `tagged` int(11) DEFAULT NULL,
   KEY `image_id` (`image_id`),
   KEY `tagged` (`tagged`),
   CONSTRAINT `tags_ibfk_1` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`),
@@ -91,7 +91,7 @@ CREATE TABLE `tags` (
 
 LOCK TABLES `tags` WRITE;
 /*!40000 ALTER TABLE `tags` DISABLE KEYS */;
-INSERT INTO `tags` VALUES ('img1','usera'),('img1','userb'),('img1','userc'),('img2','userb'),('img2','userc'),('img3','userd'),('img4','userb'),('img2','usera'),('img5','userb');
+INSERT INTO `tags` VALUES (1,9),(4,9),(8,9),(3,8),(6,8),(1,3),(6,5),(6,2),(6,4);
 /*!40000 ALTER TABLE `tags` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,10 +103,10 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` varchar(10) NOT NULL DEFAULT '',
-  `name` varchar(10) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -115,7 +115,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('usera','manish'),('userb','hiten'),('userc','jutin'),('userd','ajay'),('usere','rumi');
+INSERT INTO `users` VALUES (1,'raj'),(2,'hari'),(3,'sham'),(4,'kumar'),(5,'kishore'),(6,'ankit'),(7,'sahil'),(8,'chetna'),(9,'suru');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -128,4 +128,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-09-11  0:08:50
+-- Dump completed on 2013-09-19 18:54:59
