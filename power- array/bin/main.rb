b7dfda73fb9743f296cc6b06750229cef363d094
name <name@email.com>
2013-09-23 10:36:40 +0530

    deleted

--- power- array/bin/main.rb
+++
@@ -1,2 +0,0 @@
-require_relative '../lib/array.rb'
-p [1, 2, 3, 4, 5, 6].power(3)
--- power- array/lib/array.rb
+++
@@ -1,5 +0,0 @@
-class Array
-  def power(exponent)
-    self.each_with_index { |value, index| self[index] = value ** exponent }
-  end
-end
