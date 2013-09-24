36adb813b99f247459f2b7d7f22354d328f21057
name <name@email.com>
2013-09-24 15:31:06 +0530

    corrected

--- MySQL_exercise/users friends tags/dump.sql
+++ MySQL_exercise/users friends tags/dump.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (i686)
 --
--- Host: localhost    Database: users_tags
+-- Host: localhost    Database: users_tag
 -- ------------------------------------------------------
 -- Server version	5.5.32-0ubuntu0.12.10.1
 
@@ -23,13 +23,15 @@ DROP TABLE IF EXISTS `friends`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `friends` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL DEFAULT '0',
   `friend` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`user_id`,`friend`),
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
   KEY `friend` (`friend`),
   CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `friends_ibfk_2` FOREIGN KEY (`friend`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +40,7 @@ CREATE TABLE `friends` (
 
 LOCK TABLES `friends` WRITE;
 /*!40000 ALTER TABLE `friends` DISABLE KEYS */;
-INSERT INTO `friends` VALUES (2,1),(3,1),(9,1),(1,2),(4,2),(6,2),(9,2),(1,3),(5,3),(8,3),(2,4),(6,4),(9,4),(3,5),(6,5),(8,5),(9,5),(2,6),(4,6),(5,6),(8,6),(9,6),(8,7),(3,8),(5,8),(6,8),(7,8),(1,9),(2,9),(4,9),(5,9),(6,9);
+INSERT INTO `friends` VALUES (1,2,1),(2,3,1),(3,9,1),(4,1,2),(5,4,2),(6,6,2),(7,9,2),(8,1,3),(9,5,3),(10,8,3),(11,2,4),(12,6,4),(13,9,4),(14,3,5),(15,6,5),(16,8,5),(17,9,5),(18,2,6),(19,4,6),(20,5,6),(21,8,6),(22,9,6),(23,8,7),(24,3,8),(25,5,8),(26,6,8),(27,7,8),(28,1,9),(29,2,9),(30,4,9),(31,5,9),(32,6,9);
 /*!40000 ALTER TABLE `friends` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -64,7 +66,7 @@ CREATE TABLE `images` (
 
 LOCK TABLES `images` WRITE;
 /*!40000 ALTER TABLE `images` DISABLE KEYS */;
-INSERT INTO `images` VALUES (1,1),(4,2),(8,2),(3,3),(6,5),(5,8),(2,9),(7,9),(9,9);
+INSERT INTO `images` VALUES (1,1),(2,2),(3,2),(4,3),(5,5),(8,6),(9,6),(6,8),(7,9);
 /*!40000 ALTER TABLE `images` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -76,13 +78,15 @@ DROP TABLE IF EXISTS `tags`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `image_id` int(11) DEFAULT NULL,
   `tagged` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `image_id` (`image_id`),
   KEY `tagged` (`tagged`),
   CONSTRAINT `tags_ibfk_1` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`),
   CONSTRAINT `tags_ibfk_2` FOREIGN KEY (`tagged`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,7 +95,7 @@ CREATE TABLE `tags` (
 
 LOCK TABLES `tags` WRITE;
 /*!40000 ALTER TABLE `tags` DISABLE KEYS */;
-INSERT INTO `tags` VALUES (1,9),(4,9),(8,9),(3,8),(6,8),(1,3),(6,5),(6,2),(6,4);
+INSERT INTO `tags` VALUES (1,1,9),(2,4,9),(3,8,9),(4,3,8),(5,6,8),(6,1,3),(7,6,5),(8,6,2),(9,6,4);
 /*!40000 ALTER TABLE `tags` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -128,4 +132,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-09-19 18:54:59
+-- Dump completed on 2013-09-24 15:29:25
