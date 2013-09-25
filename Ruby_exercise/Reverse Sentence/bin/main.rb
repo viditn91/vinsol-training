8119b2074a947a255201b24b09fae95be218d9f1
name <name@email.com>
2013-09-25 21:59:56 +0530

    added exercise

---
+++ Ruby_exercise/Reverse Sentence/bin/main.rb
@@ -0,0 +1,3 @@
+require_relative '../lib/string.rb'
+string = String.new("An apple a day keeps the doctor away")
+p string.reverse_sentence
---
+++ Ruby_exercise/Reverse Sentence/lib/string.rb
@@ -0,0 +1,5 @@
+class String
+  def reverse_sentence
+    split.reverse.join(' ')
+  end
+end
