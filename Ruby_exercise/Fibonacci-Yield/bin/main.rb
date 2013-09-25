7ef100374bcaa8e5aeb7eae75e55b3bed8d1b491
name <name@email.com>
2013-09-25 11:28:39 +0530

    used yield

--- Ruby_exercise/Fibonacci-Yield/bin/main.rb
+++ Ruby_exercise/Fibonacci-Yield/bin/main.rb
@@ -1,5 +1,5 @@
 require_relative '../lib/fibonacci.rb'
 
 new_series = Fibonacci.new
-new_series.fibonacci_upto(1000)
+new_series.fibonacci_upto(1000) { |results, n| results[n] = n > 1? (results[n - 2] + results[n - 1]) : n }
 p new_series.show_results
--- Ruby_exercise/Fibonacci-Yield/lib/fibonacci.rb
+++ Ruby_exercise/Fibonacci-Yield/lib/fibonacci.rb
@@ -1,10 +1,10 @@
 class Fibonacci
   def initialize
-  	@results = Array.new
+    @results = Array.new
   end
   def fibonacci_upto(limit)
     0.upto(limit - 1) do |n|
-      @results[n] = n > 1? (@results[n - 2] + @results[n - 1]) : n 		
+      yield(@results, n)
     end
   end
   def show_results
