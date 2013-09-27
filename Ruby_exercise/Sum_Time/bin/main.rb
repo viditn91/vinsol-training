c13993b26c32d009dfcd995755a8da32a3b8f6a3
name <name@email.com>
2013-09-27 22:50:55 +0530

    added exercise

--- Ruby_exercise/Sum_Time/bin/main.rb
+++ Ruby_exercise/Sum_Time/bin/main.rb
@@ -1,4 +1,3 @@
 require_relative '../lib/string.rb'
-string1 = String.new("11:23:07")
-string2 = "23:53:45"
-p string1.sum_time(string2)
+times = String.new("11:23:07","23:53:45")
+p times.sum_time
--- Ruby_exercise/Sum_Time/lib/string.rb
+++ Ruby_exercise/Sum_Time/lib/string.rb
@@ -1,25 +1,42 @@
 class String
-  def sum_time(other_string)
+  def initialize(time1, time2)
+    @md = [match(time1), match(time2)]
+  end
+
+  def sum_time
+    if @md.include?(nil)
+      "enter correct time_"
+    else
+      secs = 0
+      mins = 0
+      hours = 0
+      0.upto(@md.size - 1) do |i|
+        secs  += @md[i][:sec].to_i
+        mins  += @md[i][:min].to_i
+        hours += @md[i][:hour].to_i
+      end
+      mins += secs / 60
+      hours += mins / 60
+      days = hours / 24
+      display_time(secs, mins, hours, days)
+    end  
+  end
+
+  def match(string)
     pattern = %r{
       (?<hour> [0-1]\d | [2][0-3] | ^\d(?=:)){0}
       (?<min>  [0-5]\d | (?<=:)\d){0}  
       (?<sec>  [0-5]\d | (?<=:)\d){0}
       \g<hour>:\g<min>:\g<sec>
     }x
-    md1 = pattern.match(self)
-    md2 = pattern.match(other_string)
-    if md1 != nil && md2 != nil
-      secs  = md1[:sec].to_i + md2[:sec].to_i
-      mins  = md1[:min].to_i + md2[:min].to_i + secs / 60
-      hours = md1[:hour].to_i + md2[:hour].to_i + mins / 60
-      days  = hours / 24
-      if days > 0
-        "#{ days } Days & #{ (hours % 24).to_s.rjust(2,'0') }:#{ (mins % 60).to_s.rjust(2,'0') }:#{ (secs % 60).to_s.rjust(2,'0') }"
-      else
-        "#{ (hours % 24).to_s.rjust(2,'0') }:#{ (mins % 60).to_s.rjust(2,'0') }:#{ (secs % 60).to_s.rjust(2,'0') }"
-      end
+    return pattern.match(string)
+  end
+
+  def display_time(secs, mins, hours, days)
+    if days > 0
+      "#{ days } Days & #{ (hours % 24).to_s.rjust(2,'0') }:#{ (mins % 60).to_s.rjust(2,'0') }:#{ (secs % 60).to_s.rjust(2,'0') }"
     else
-      "Please enter correct time_"
+      "#{ (hours % 24).to_s.rjust(2,'0') }:#{ (mins % 60).to_s.rjust(2,'0') }:#{ (secs % 60).to_s.rjust(2,'0') }"
     end
   end
 end
