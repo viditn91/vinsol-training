4197c70bc215fbbe70afd0fea65505a17be7c3c6
name <name@email.com>
2013-09-30 17:02:18 +0530

    corrected

--- Ruby_exercise/Interest_Difference/lib/interest.rb
+++ Ruby_exercise/Interest_Difference/lib/interest.rb
@@ -1,20 +1,15 @@
 class Interest
-  def initialize(principal, time, rate, &block)
-    @p = principal.to_i
-    @t = time.to_i
-    @r = rate.to_f
-    @block = block 
-  end
+  attr_accessor :p, :t, :r
 
   def get_difference
-    "difference between simple and compound interest is #{ @block.call(cal_simpleInt, cal_compInt) }"
+    "difference between simple and compound interest is #{ cal_compInt - cal_simpleInt }"
   end
 
   def cal_simpleInt
-    return (1 + (@r * @t) / 100) * @p
+    return (1 + r * t / 100) * p
   end
 
   def cal_compInt
-    return (1 + (@r) / 100) ** @t * @p
+    return (1 + r / 100) ** t * p
   end
 end
