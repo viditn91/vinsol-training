d4be7364c2e8220934d570baccf04e014421deb5
name <name@email.com>
2013-09-30 10:19:18 +0530

    used attr_writer, removed modify_price

--- Ruby_exercise/vehicle- subclass/lib/vehicle.rb
+++ Ruby_exercise/vehicle- subclass/lib/vehicle.rb
@@ -1,13 +1,11 @@
 class Vehicle
+  attr_writer :price
+
   def initialize(name, price)
     @name = name
     @price = price    
   end
-
-  def modify_price(price)
-    @price = price
-  end
-
+  
   def to_s
     "name:#{ @name } and price:#{ @price }"
   end
