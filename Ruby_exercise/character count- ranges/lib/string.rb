dd8dee8dd2cc42dd206e1e034706b8660cbb3e03
name <name@email.com>
2013-09-30 10:24:11 +0530

    optimized

--- Ruby_exercise/character count- ranges/lib/string.rb
+++ Ruby_exercise/character count- ranges/lib/string.rb
@@ -1,15 +1,16 @@
 class String
   def count_range
     character_count = Hash.new(0) 
+    lowcase, upcase, digit = 'a'..'z', 'A'..'Z', '0'..'9'
     self.each_char do |char|
       case char
-      when 'a'..'z'
+      when lowcase
         character_count["lowercase_count"] += 1
-      when 'A'..'Z'
+      when upcase
         character_count["uppercase_count"] += 1
-      when '0'..'9'
+      when digit
         character_count["numbers_count"] += 1
-      else 
+      else
         character_count["special_count"] += 1
       end
     end
