1ef1f3f21295735f31463928793c6d912fd265c3
vidit <vidit@vinsol.com>
2013-10-14 19:12:40 +0530

    no more string returning methods

--- Ruby_exercise/Factorial-Ranges/bin/main.rb
+++ Ruby_exercise/Factorial-Ranges/bin/main.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/integer.rb'
 puts "enter a number_"
 number = gets.to_i
-p number.factorial
+factorial = number.factorial
+puts factorial ? "factorial of #{ number } is #{ factorial }" : "factorial does not exist"
--- Ruby_exercise/Factorial-Ranges/lib/integer.rb
+++ Ruby_exercise/Factorial-Ranges/lib/integer.rb
@@ -1,5 +1,5 @@
 class Integer
   def factorial
-    "factorial : #{ self > 0 ? (1..self).to_a.inject(:*) : self == 0 ? 1 : "does not exist" }"  
+    self > 0 ? (1..self).to_a.inject(:*) : self == 0 ? 1 : false
   end
 end
